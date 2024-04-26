bbf795d2610ebdbdf8827929ac70712534321662
Diogo Freire <diogofre@gmail.com>
2024-04-26 15:52:16 -0300

    Definição de títulos nos arquivos de exportação

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return Collection
@@ -14,5 +15,20 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return auth()->user()->tarefas()->get();
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'ID Tarefa',
+            'ID Usuário',
+            'Tarefa',
+            'Data Limite Conclusão',
+            'Data criação',
+            'Data Atualização'
+        ];
     }
 }
